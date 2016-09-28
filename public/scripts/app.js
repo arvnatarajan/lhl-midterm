18b210cc0b482cd9bb0fa493842467a62a9f13db
Arvind Natarajan <Arvind@Arvs-MBP.local>
2016-09-28 18:18:46 -0400

    added initial sidebar and some partials

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,13 @@ $(() => {
     }
   });;
 });
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
