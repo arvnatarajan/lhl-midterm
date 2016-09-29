83c2be3e98128675bcea4182ce73f205cf054a27
Arvind Natarajan <Arvind@Arvs-MBP.local>
2016-09-29 16:05:15 -0400

    updating map.js file

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -2,29 +2,47 @@ let map;
 let points;
 let currentLat;
 let currentLong;
-let icon ="";
-
 let contentString = `
   <div id="content">
   <p>This is my content</p>
   </div>
 `;
 
-function geo_success(position) {
-  console.log(`Current Position: ${position.coords.latitude} ${position.coords.longitude}`);
-  currentLat = position.coords.latitude;
-  currentLong = position.coords.longitude
-}
-function geo_error() {
-  alert("Sorry, no position available.");
-}
-var geo_options = {
+let geo_options = {
   enableHighAccuracy: true,
   maximumAge        : 30000,
   timeout           : 27000
 };
+let icon ="";
 
-var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+const iconBase = 'http://maps.google.com/mapfiles/';
+
+const icons = {
+  '1': {
+    icon: iconBase + 'kml/pal2/icon4.png'
+  },
+  '2': {
+    icon: iconBase + 'kml/pal2/icon19.png'
+  },
+  '3': {
+    icon: iconBase + 'kml/pal2/icon32.png'
+  },
+  '4': {
+    icon: iconBase + 'kml/pal4/icon38.png'
+  }
+}
+
+const geo_success = (position) => {
+  console.log(`Current Position: ${position.coords.latitude} ${position.coords.longitude}`);
+  currentLat = position.coords.latitude;
+  currentLong = position.coords.longitude;
+}
+
+const geo_error = () => {
+  alert("Sorry, no position available.");
+}
+
+const wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 
 const infowindow = new google.maps.InfoWindow({
   content: contentString
@@ -32,22 +50,6 @@ const infowindow = new google.maps.InfoWindow({
 
 const accessPoints = $.get('/api/points');
 
-const iconBase = 'http://maps.google.com/mapfiles/ms/icons/';
-
-const icons = {
-  '1': {
-    icon: iconBase + 'green.png'
-  },
-  '2': {
-    icon: iconBase + 'blue.png'
-  },
-  '3': {
-    icon: iconBase + 'red.png'
-  },
-  '4': {
-    icon: iconBase + 'yellow.png'
-  }
-}
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -75,13 +77,11 @@ const addMarker = (point, timeout) => {
 //   result = [];
 // }
 
-// const addInfoWindow = (result) => {
-//   result.forEach((point) => {
-//     point.addListener('click', () => {
-//        infowindow.open(map, point);
-//     });
-//   });
-// }
+const addInfoWindow = (point) => {
+  point.addListener('click', () => {
+     infowindow.open(map, point);
+  });
+}
 
 initMap();
 // clearMarkers();
@@ -89,7 +89,7 @@ initMap();
 
 accessPoints.then((result) => {
   for (let i = 0; i < result.length; i++) {
-    addMarker(result[i], i * 1000);
+    addMarker(result[i], i * 200);
   }
 })
 .catch((err) => {
