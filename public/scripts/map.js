73aa04067f94f1fb33a2ba3ed7889213a2a831b3
Arvind Natarajan <Arvind@Arvs-MBP.local>
2016-09-29 14:42:17 -0400

    distinguishes points on different lists

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,43 +1,78 @@
 let map;
-let points;
+let icon ="";
 
-let contentString = `
+const contentString = `
   <div id="content">
   <p>This is my content</p>
   </div>
 `;
+
+const infowindow = new google.maps.InfoWindow({
+  content: contentString
+});
+
+const accessPoints = $.get('/api/points');
+
+const iconBase = 'http://maps.google.com/mapfiles/ms/icons/';
+
+const icons = {
+  '1': {
+    icon: iconBase + 'green.png'
+  },
+  '2': {
+    icon: iconBase + 'blue.png'
+  },
+  '3': {
+    icon: iconBase + 'red.png'
+  },
+  '4': {
+    icon: iconBase + 'yellow.png'
+  }
+}
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: {lat: 43.65, lng: -79.36}
   });
-
-  $.get('/api/points')
-    .then((result) => {
-      result.forEach( (coord) => {
-
-        let numCoord = {
-          lat: Number(coord.lat),
-          lng: Number(coord.lng)
-        }
-
-        let infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-
-        let marker = new google.maps.Marker({
-          position: numCoord,
-          map: map,
-          title: 'Hello World!'
-        });
-
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 }
+
+const addMarker = (point, timeout) => {
+
+  window.setTimeout(() => {
+    let marker = new google.maps.Marker({
+      position: new google.maps.LatLng(Number(point.lat), Number(point.lng)),
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: icons[point['list_id']].icon
+    });
+  }, timeout);
+}
+
+// const clearMarkers = (result) => {
+//   for (var i = 0; i < result.length; i++) {
+//     result[i].setMap(null);
+//   }
+//   result = [];
+// }
+
+// const addInfoWindow = (result) => {
+//   result.forEach((point) => {
+//     point.addListener('click', () => {
+//        infowindow.open(map, point);
+//     });
+//   });
+// }
+
+initMap();
+// clearMarkers();
+// addInfoWindow();
+
+accessPoints.then((result) => {
+  for (let i = 0; i < result.length; i++) {
+    addMarker(result[i], i * 1000);
+  }
+})
+.catch((err) => {
+  console.log(err);
+});
