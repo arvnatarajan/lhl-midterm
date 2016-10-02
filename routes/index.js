65f8bdeee1bcdf7b1c2e118cd60ce503ec7ad428
Arvind Natarajan <Arvind@Arvs-MBP.local>
2016-10-02 19:20:19 -0400

    adding reference to profil pic url

--- routes/index.js
+++ routes/index.js
@@ -43,12 +43,14 @@ router.get('/editlist', function(req, res, next) {
 /* GET profile page */
 router.get('/profile', isLoggedIn, function(req, res) {
   // render profile page, get user info from session
+  console.log(req.user[0]);
   res.render('profile.ejs', {
     user_id: req.user[0].id,
     username: req.user[0].username,
     first_name: req.user[0].first_name,
     last_name: req.user[0].last_name,
-    email: req.user[0].email
+    email: req.user[0].email,
+    picture: req.user[0].picture
    });
 });
 
