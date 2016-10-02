59ce82951040495b5e399b37f99bdd2684169278
EricCasey <ericcasey509@gmail.com>
2016-10-02 16:17:34 -0400

    removing conflict

--- public/scripts/likeHandler.js
+++ public/scripts/likeHandler.js
@@ -17,14 +17,12 @@ $(document).ready(() => {
 
 */
 
-<<<<<<< HEAD
 // changes the color of the heart by adding 'liked' class
     $(".glyphicon-heart-empty").on("click", function(){
         console.log('liked')
         $(".glyphicon-heart-empty").toggleClass("liked");
     });
-=======
->>>>>>> 351594c2bdcf13512197fa18b51828c339866901
+
 
 $(".glyphicon-heart-empty").on("click", function(){
    /* console.log($(this).data('likeid'));*/
